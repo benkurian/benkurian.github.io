67a982ccb799759890048fd6d4490ad83841d40a
Benjamin Kurian <94756966+benkurian@users.noreply.github.com>
2024-03-28 01:13:20 -0400

    Updated scripts with comments

--- assets/data/music.js
+++ assets/data/music.js
@@ -1,37 +1,41 @@
+// Array containing information about track/album information
 var music = [{
-    title: "Christmas Favorites on Piano and Violin",
-    description: "The first album I created on December 23, 2023.",
-    img: "/assets/images/music/christmasfavorites.jpeg",
-    linkText: "Listen on <i class='bi bi-spotify'></i> Spotify",
-    linkHref: "https://open.spotify.com/album/37wLcBz62t7JhlQjcCcqvp?si=opEh7L2bSZaMEmgJwvzGPQ",
-    dateAdded: "December 23, 2023",
- },
- {
-    title: "Who Do I Want to Be? Soundtrack",
-    description: "The music to my StudentCam 2024 documentary.",
-    img: "/assets/images/music/wdiwtb.jpeg",
-    linkText: "Listen on <i class='bi bi-youtube'></i> YouTube",
-    linkHref: "https://youtu.be/FUbPylyq6J0",
-    dateAdded: "March 26, 2024",
- },
-];
+  title: "Christmas Favorites on Piano and Violin",
+  description: "The first album I created on December 23, 2023.",
+  img: "/assets/images/music/christmasfavorites.jpeg",
+  linkText: "Listen on <i class='bi bi-spotify'></i> Spotify",
+  linkHref: "https://open.spotify.com/album/37wLcBz62t7JhlQjcCcqvp?si=opEh7L2bSZaMEmgJwvzGPQ",
+  dateAdded: "December 23, 2023",
+},
+{
+  title: "Who Do I Want to Be? Soundtrack",
+  description: "The music to my StudentCam 2024 documentary.",
+  img: "/assets/images/music/wdiwtb.jpeg",
+  linkText: "Listen on <i class='bi bi-youtube'></i> YouTube",
+  linkHref: "https://youtu.be/FUbPylyq6J0",
+  dateAdded: "March 26, 2024",
+}];
 
+// String to store HTML for displaying music cards
 var cardContainerHTML = "";
 
+// Loop through each music track or album in reverse order and construct HTML for each card
 for (var i = music.length - 1; i >= 0; i--) {
- cardContainerHTML += `
-    <div class='card m-3 shadow animate__animated animate__fadeIn' style='width: 18rem; height: 33rem;'>
-      <img oncontextmenu="return false;" src='${music[i].img}' class='mt-3 rounded w-100 h-100' alt='${music[i].title}' style='object-fit: cover;'>
-      <div class='card-body pb-0'>
-        <h5 class='card-title'>${music[i].title}</h5>
-        <p class='card-text'>${music[i].description}</p>
-        <a href='${music[i].linkHref}' class='btn btn-dark' target='_blank' rel='noopener noreferrer'>
-          ${music[i].linkText}
-        </a>
-        <hr>
-        <p class='text-secondary'>${music[i].dateAdded}</p>
-      </div>
+  cardContainerHTML += `
+  <div class='card m-3 shadow animate__animated animate__fadeIn' style='width: 18rem; height: 33rem;'>
+    <img oncontextmenu="return false;" src='${music[i].img}' class='mt-3 rounded w-100 h-100' alt='${music[i].title}' style='object-fit: cover;'>
+    <div class='card-body pb-0'>
+      <h5 class='card-title'>${music[i].title}</h5>
+      <p class='card-text'>${music[i].description}</p>
+      <a href='${music[i].linkHref}' class='btn btn-dark' target='_blank' rel='noopener noreferrer'>
+        ${music[i].linkText}
+      </a>
+      <hr>
+      <p class='text-secondary'>${music[i].dateAdded}</p>
     </div>
-  `;
+  </div>
+`;
 }
+
+// Add the constructed HTML to the card container in the document
 document.getElementById("cardContainer").innerHTML = cardContainerHTML;
--- assets/data/videos.js
+++ assets/data/videos.js
@@ -1,12 +1,13 @@
+// Array containing existing video information
 var videos = [
   {
-  title: "Aquaponics Video",
-  description: "First-prize winner of World of 7 Billion 2022.",
-  img: "/assets/images/videos/aquaponics.jpeg",
-  linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
-  linkHref: "https://youtu.be/zsHRN33V4IA",
-  dateAdded: "May 9, 2022",
-  }, 
+    title: "Aquaponics Video",
+    description: "First-prize winner of World of 7 Billion 2022.",
+    img: "/assets/images/videos/aquaponics.jpeg",
+    linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
+    linkHref: "https://youtu.be/zsHRN33V4IA",
+    dateAdded: "May 9, 2022",
+  },
   {
     title: "Losing A Generation",
     description: "First-prize winner of C-SPAN StudentCam 2023.",
@@ -14,7 +15,7 @@ var videos = [
     linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
     linkHref: "https://youtu.be/-rcz3fX9laQ",
     dateAdded: "March 15, 2023",
-  }, 
+  },
   {
     title: "E-Waste Video",
     description: "Second-prize winner of World of 8 Billion 2023.",
@@ -22,7 +23,7 @@ var videos = [
     linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
     linkHref: "https://youtu.be/d1BNiJjgWQ8",
     dateAdded: "May 8, 2023",
-  }, 
+  },
   {
     title: "COMA Cricket Tourney",
     description: "A 1-min commercial I made for COMA's Cricket Tournament.",
@@ -30,7 +31,7 @@ var videos = [
     linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
     linkHref: "https://youtu.be/FI9LKZP8Ppc",
     dateAdded: "July 15, 2023",
-  }, 
+  },
   {
     title: "Who Do I Want to Be?",
     description: "Second-prize winner of C-SPAN StudentCam 2024.",
@@ -41,22 +42,26 @@ var videos = [
   },
 ];
 
+// String to store HTML for displaying video cards
 var cardContainerHTML = "";
 
+// Loop through each video in reverse order and construct HTML for each video card
 for (var i = videos.length - 1; i >= 0; i--) {
   cardContainerHTML += `
-     <div class='card m-3 shadow animate__animated animate__fadeIn' style='width: 18rem; height: 33rem;'>
-       <img oncontextmenu="return false;" src='${videos[i].img}' class='mt-3 rounded w-100 h-100' alt='${videos[i].title}' style='object-fit: cover;'>
-       <div class='card-body pb-0'>
-         <h5 class='card-title'>${videos[i].title}</h5>
-         <p class='card-text'>${videos[i].description}</p>
-         <a href='${videos[i].linkHref}' class='btn btn-dark' target='_blank' rel='noopener noreferrer'>
-           ${videos[i].linkText}
-         </a>
-         <hr>
-         <p class='text-secondary'>${videos[i].dateAdded}</p>
-       </div>
-     </div>
-   `;
+    <div class='card m-3 shadow animate__animated animate__fadeIn' style='width: 18rem; height: 33rem;'>
+      <img oncontextmenu="return false;" src='${videos[i].img}' class='mt-3 rounded w-100 h-100' alt='${videos[i].title}' style='object-fit: cover;'>
+      <div class='card-body pb-0'>
+        <h5 class='card-title'>${videos[i].title}</h5>
+        <p class='card-text'>${videos[i].description}</p>
+        <a href='${videos[i].linkHref}' class='btn btn-dark' target='_blank' rel='noopener noreferrer'>
+          ${videos[i].linkText}
+        </a>
+        <hr>
+        <p class='text-secondary'>${videos[i].dateAdded}</p>
+      </div>
+    </div>
+  `;
 }
+
+// Add the constructed HTML to the card container in the document
 document.getElementById("cardContainer").innerHTML = cardContainerHTML;
