4f7a41e60cc5150ca915815407ae516a002903cb
Benjamin Kurian <94756966+benkurian@users.noreply.github.com>
2024-03-28 11:42:19 -0400

    Added original media, optimized images for web

--- assets/data/music.js
+++ assets/data/music.js
@@ -2,7 +2,7 @@
 var music = [{
   title: "Christmas Favorites on Piano and Violin",
   description: "The first album I created on December 23, 2023.",
-  img: "/assets/images/music/christmasfavorites.jpeg",
+  img: "christmasfavorites.webp",
   linkText: "Listen on <i class='bi bi-spotify'></i> Spotify",
   linkHref: "https://open.spotify.com/album/37wLcBz62t7JhlQjcCcqvp?si=opEh7L2bSZaMEmgJwvzGPQ",
   dateAdded: "December 23, 2023",
@@ -10,7 +10,7 @@ var music = [{
 {
   title: "Who Do I Want to Be? Soundtrack",
   description: "The music to my StudentCam 2024 documentary.",
-  img: "/assets/images/music/wdiwtb.jpeg",
+  img: "wdiwtb.webp",
   linkText: "Listen on <i class='bi bi-youtube'></i> YouTube",
   linkHref: "https://youtu.be/FUbPylyq6J0",
   dateAdded: "March 26, 2024",
@@ -23,7 +23,7 @@ var cardContainerHTML = "";
 for (var i = music.length - 1; i >= 0; i--) {
   cardContainerHTML += `
   <div class='card m-3 shadow animate__animated animate__fadeIn' style='width: 18rem; height: 33rem;'>
-    <img oncontextmenu="return false;" src='${music[i].img}' class='mt-3 rounded w-100 h-100' alt='${music[i].title}' style='object-fit: cover;'>
+    <img oncontextmenu="return false;" loading="lazy" src='/assets/images/music/compressed/${music[i].img}' class='mt-3 rounded w-100 h-100' alt='${music[i].title}' style='object-fit: cover;'>
     <div class='card-body pb-0'>
       <h5 class='card-title'>${music[i].title}</h5>
       <p class='card-text'>${music[i].description}</p>
--- assets/data/videos.js
+++ assets/data/videos.js
@@ -3,7 +3,7 @@ var videos = [
   {
     title: "Aquaponics Video",
     description: "First-prize winner of World of 7 Billion 2022.",
-    img: "/assets/images/videos/aquaponics.jpeg",
+    img: "aquaponics.webp",
     linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
     linkHref: "https://youtu.be/zsHRN33V4IA",
     dateAdded: "May 9, 2022",
@@ -11,7 +11,7 @@ var videos = [
   {
     title: "Losing A Generation",
     description: "First-prize winner of C-SPAN StudentCam 2023.",
-    img: "/assets/images/videos/lag.jpeg",
+    img: "lag.webp",
     linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
     linkHref: "https://youtu.be/-rcz3fX9laQ",
     dateAdded: "March 15, 2023",
@@ -19,7 +19,7 @@ var videos = [
   {
     title: "E-Waste Video",
     description: "Second-prize winner of World of 8 Billion 2023.",
-    img: "/assets/images/videos/ewaste.jpeg",
+    img: "ewaste.webp",
     linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
     linkHref: "https://youtu.be/d1BNiJjgWQ8",
     dateAdded: "May 8, 2023",
@@ -27,7 +27,7 @@ var videos = [
   {
     title: "COMA Cricket Tourney",
     description: "A 1-min commercial I made for COMA's Cricket Tournament.",
-    img: "/assets/images/videos/coma.jpeg",
+    img: "coma.webp",
     linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
     linkHref: "https://youtu.be/FI9LKZP8Ppc",
     dateAdded: "July 15, 2023",
@@ -35,7 +35,7 @@ var videos = [
   {
     title: "Who Do I Want to Be?",
     description: "Second-prize winner of C-SPAN StudentCam 2024.",
-    img: "/assets/images/videos/wdiwtb.jpeg",
+    img: "wdiwtb.webp",
     linkText: "Watch on <i class='bi bi-youtube'></i> YouTube",
     linkHref: "https://youtu.be/ju_pT2KA4v0",
     dateAdded: "March 13, 2024",
@@ -49,7 +49,7 @@ var cardContainerHTML = "";
 for (var i = videos.length - 1; i >= 0; i--) {
   cardContainerHTML += `
     <div class='card m-3 shadow animate__animated animate__fadeIn' style='width: 18rem; height: 33rem;'>
-      <img oncontextmenu="return false;" src='${videos[i].img}' class='mt-3 rounded w-100 h-100' alt='${videos[i].title}' style='object-fit: cover;'>
+      <img oncontextmenu="return false;" loading="lazy" src='/assets/images/videos/compressed/${videos[i].img}' class='mt-3 rounded w-100 h-100' alt='${videos[i].title}' style='object-fit: cover;'>
       <div class='card-body pb-0'>
         <h5 class='card-title'>${videos[i].title}</h5>
         <p class='card-text'>${videos[i].description}</p>
