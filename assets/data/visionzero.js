dd68144ceba8fb3fc7630d5bffdc5eda78a5039c
Benjamin Kurian <94756966+benkurian@users.noreply.github.com>
2025-01-30 16:46:07 -0500

    Added data for LLS and VisionZero

---
+++ assets/data/visionzero.js
@@ -0,0 +1,77 @@
+var watch = `
+            <h1>Watch the Documentary</h1><br>
+            <iframe loading="lazy" class="rounded shadow" src="https://www.youtube.com/embed/h1dHoLHaSDc?si=T95yyr9gnTPJfTYV" width="100%" height="800px" allowfullscreen></iframe>
+            `;
+var summary = `
+            <h1>Executive Summary</h1>
+            <p class="lead">Every year, nearly 43,000 lives are lost to traffic crashes in the United States, with millions more injured. This crisis isn't just a headline—it’s deeply personal, impacting families, friends, and communities. Achieving Vision Zero, the goal of eliminating all roadway fatalities, demands bold action and transformative change.</p>
+            <p class="lead">For decades, road safety has relied on a reactive approach, addressing danger only after lives have been lost. The Safe System framework, endorsed by NHTSA, shifts the focus to proactive strategies. By emphasizing safer people, safer roads, safer vehicles, safer speeds, and post-crash care, it acknowledges that human error is inevitable but doesn't have to result in fatalities.</p>     
+            <p class="lead">Artificial intelligence builds on this framework, offering unparalleled opportunities to analyze data and act swiftly. AI can monitor driving behaviors, identify hazardous conditions, and optimize traffic flow in real time. It enhances vehicle safety through advanced systems like auto-braking and lane-assist and supports faster emergency response through predictive analytics. This technology doesn’t replace human responsibility—it complements it, providing tools to prevent crashes and save lives.</p>
+            <p class="lead">However, significant challenges remain. AI systems rely on high-quality, unbiased data, yet issues like incomplete data and privacy concerns undermine their potential. Legal uncertainties and public skepticism, particularly around autonomous vehicles, pose barriers to adoption. Addressing these challenges requires collaboration across governments, industry, and communities, alongside measures like balanced datasets, transparent AI processes, and stronger privacy protections.</p>
+            <p class="lead">Achieving Vision Zero also demands a cultural shift. Road safety isn't just about technology—it’s about changing behaviors and fostering accountability among all road users. Together, the integration of AI into the Safe System framework and a commitment to responsible driving decisions will create a safer environment for everyone.</p>
+            <p class="lead">We stand at a crossroads. The cost of inaction isn’t just measured in dollars, but in human lives. The choices made today—embracing innovation, supporting policy change, and valuing safety—will determine whether Vision Zero becomes a reality. It starts and ends with us.</p>
+            `;
+var workscited = `
+            <h1>Works Cited</h1><br>
+            <iframe height="1000px" src="https://docs.google.com/document/d/e/2PACX-1vRUJ0k1XS-jvyJ7YzlZtNIwdWvo-ESU39R9raLINkgVDKSJA9sj4olppeamw5_jSM4aqhbsHDUgev5I/pub?embedded=true"></iframe>
+            `;
+var interviews = `
+            <p class="lead">Working on editing through ten 30-40 minute interviews. Hopefully should be available to watch in a few days!</p>
+            `;
+var music = `
+            <h1>Soundtrack</h1><br>
+            <iframe loading="lazy" class="rounded shadow" src="https://www.youtube.com/embed/2vo7PdbGszw?si=uaAT58LusiAPsJRw" width="100%" height="800px" allowfullscreen></iframe>
+            `;
+var acknowledgements = `
+            <h1>Acknowlegements</h1><br>
+            <h2>Interviewees</h2>
+            <p class="lead">Ferzan Ahmed - Executive Director, Ohio Turnpike Commission, 2nd Vice President, IBTTA</p>
+            <p class="lead">Michael Oberdorf - Assistant Foreman, Ohio Turnpike Commission, Crash Survivor</p>
+            <p class="lead">Staff Lieutenant Richard Reeder - Turnpike Liasion, Ohio State Highway Patrol</p>
+            <p class="lead">Rafael Hernandez - Vice President, Tolling Solutions, Kyra Solutions</p>
+            <p class="lead">Dr. Mihail Chirita - AI Consultant, Sagacitas Technologies</p>
+            <p class="lead">Michelle May - Highway Safety Program Manager, Ohio Department of Transportation</p>
+            <p class="lead">David Doria - Director of Engineering for Automated Driving, Magna Electronics</p>
+            <p class="lead">Ben Levine - Deputy Assistant Secretary of Research and Technology, U.S. Department of Transportation</p>
+            <p class="lead">Michael Vardi - Co-Founder, CBO, Valerann</p>
+            <p class="lead">Lisa Layzell - CEO, Robotiz3d</p>
+            <h2>Olentangy Liberty High School Staff</h2>
+            <p class="lead">Mr. Hale - Math Teacher</p>
+            <p class="lead">Mr. Rothey - English Teacher</p>
+            <h2>Olentangy Liberty Middle School Staff</h2>
+            <p class="lead">Mr. Baker - Principal</p>
+            <p class="lead">Mr. Mills - 8th Grade Social Studies Teacher</p>
+            <p class="lead">Ms. Zeis - 8th Grade Science Teacher</p>
+            <p class="lead">Ms. Welsh - 7th Grade English Teacher</p>
+            <h2>Facilitators</h2>
+            <p class="lead">Aravind V.K. - Videography  Mentor</p>
+            <p class="lead">Rexon Raj - Drone Operator</p>
+            <p class="lead">Chuck Cyrill - Director of Marketing and Communications, Ohio Turnpike Commission</p>
+            <p class="lead">Zakary Sheehan - Marketing and Communications Specialist, Ohio Turnpike Commission</p>
+            <p class="lead">Matt Bruning - Press Secretary, Ohio Department of Transportation</p>
+            <p class="lead">Alex Kurian - Camera Assistant</p>
+            <p class="lead">2013 Toyota Sienna</p>
+            <p class="lead">The one guy who stopped me when filming and thought I was a celebrity</p>
+            `;
+var updates = `
+            <h1>Updates</h1><br>
+            <div class="card my-3">
+                <div class="card-header bg-primary text-white">
+                    Submission
+                </div>
+                <div class="card-body">
+                    Submitted my video to the C-SPAN StudentCam Competition! Also posted on Twitter for the #BehindTheScenes competition.
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">January 20, 2025</small>
+                </div>
+            </div>
+            `;
+            
+document.getElementById("watchContainer").innerHTML = watch; 
+document.getElementById("summaryContainer").innerHTML = summary; 
+document.getElementById("worksCitedContainer").innerHTML = workscited; 
+document.getElementById("interviewsContainer").innerHTML = interviews; 
+document.getElementById("musicContainer").innerHTML = music; 
+document.getElementById("acknowledgementsContainer").innerHTML = acknowledgements;
+document.getElementById("updatesContainer").innerHTML = updates;  
